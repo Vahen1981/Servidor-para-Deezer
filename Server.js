a7eb76f78a2e5aad9940c295a74a3e2fea5fdc92
Vahen1981 <fevahen@gmail.com>
2025-01-08 17:07:05 -0300

    cambios para server online

--- Server.js
+++ Server.js
@@ -3,13 +3,12 @@ import cors from 'cors';
 import apiRoutes from'./Proxy/Routes.js';
 
 const app = express();
-const port = 3000;
+const PORT = process.env.PORT || 3000;
 
-// Habilitar CORS para todas las rutas
-app.use(cors({ origin: 'http://localhost:5173' }));
+app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", apiRoutes);
 
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
