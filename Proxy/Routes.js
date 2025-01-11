45a44098c0f317095a09f555ecc0591f020bce70
Vahen1981 <fevahen@gmail.com>
2025-01-11 18:51:03 -0300

    new port

--- Proxy/Routes.js
+++ Proxy/Routes.js
@@ -3,7 +3,7 @@ import * as controllers from './Controllers.js';
 
 const router = express.Router();
 
-router.get('/', controllers.getGenres);
+router.get('/genres', controllers.getGenres);
 router.get('/genre/:genreId', controllers.getArtists);
 router.get('/artist/:artistId', controllers.getArtistDetail);
 router.get('/artist/tracklist/:artistId', controllers.getTrackList);
--- Server.js
+++ Server.js
@@ -5,11 +5,11 @@ import apiRoutes from'./Proxy/Routes.js';
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.use(cors({
-//   origin: 'https://project5-tuqa.onrender.com' 
-// }));
+app.use(cors({
+  origin: 'https://project5-tuqa.onrender.com' 
+}));
 
-app.use(cors());
+//app.use(cors());
 
 app.use(express.urlencoded({ extended: true }));
 app.use("/api", apiRoutes);
